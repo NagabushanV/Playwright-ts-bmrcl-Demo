489aff88732dd95d1189b1642c8460b935c71438
nagabushanv <nagabushan@vtestcorp.com>
2023-12-22 16:54:36 +0530

    demo completed

--- PageObjects/Travel-info/Travel-infopage.ts
+++ PageObjects/Travel-info/Travel-infopage.ts
@@ -10,6 +10,22 @@ export default class TravelInfopage {
     verifyFareRulesLocator: Locator;
     clickmetroTimingsLocator: Locator;
     verifymetrotimingLocator: Locator;
+    clickticketsLocator: Locator;
+    verifyTicketsLocator: Locator;
+    verifyfacilitiesatstationLocator: Locator;
+    ClickfacilitisatstationLocator: Locator;
+    clickParkingLocators: Locator;
+    verifyparkingpageLocator: Locator;
+    ClickSafetyPrecationsLocator: Locator;
+    verifySafetyPrecationsLocator: Locator;
+    clickLostandFundLocator: Locator;
+    verifyLostandFundLocator: Locator;
+    GeneralInformationLocator: Locator;
+    verifygeneralinformationLocator: Locator;
+    clicktoppingupcardsLocator: Locator;
+    verifytoppingupcardspageLocator: Locator;
+    clickNationalMobilitycardLocator: Locator;
+    verifyNationalMobilityLocator: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -19,7 +35,24 @@ export default class TravelInfopage {
         this.clickFareRulesButtonLocator = page.locator('(//a[.="Fare Rules"])[1]');
         this.verifyFareRulesLocator = page.locator("//p[.='Fare Rules']");
         this.clickmetroTimingsLocator = page.locator("(//a[.='Metro Timings'])[1]");
-        this.verifymetrotimingLocator = page.locator("(//p[.='Metro Timings'])[1]");
+        this.verifymetrotimingLocator = page.locator("//p[.='Metro Timings']");
+        this.clickticketsLocator= page.locator("(//a[.='Tickets'])[1]");
+        this.verifyTicketsLocator = page.locator("//p[.='Tickets']");
+        this.ClickfacilitisatstationLocator = page.locator("(//a[.='Facilitites at Stations'])[1]");
+        this.verifyfacilitiesatstationLocator = page.locator("//p[.='Facilitites At Stations']");
+        this.clickParkingLocators = page.locator("(//a[.='Parking'])[1]");
+        this.verifyparkingpageLocator = page.locator("//p[.='Parking']");
+        this.ClickSafetyPrecationsLocator = page.locator("(//a[.='Safety Precautions'])[1]");
+        this.verifySafetyPrecationsLocator = page.locator("//p[.='Safety Precautions']");
+        this.clickLostandFundLocator = page.locator("(//a[.='Lost & Found'])[1]");
+        this.verifyLostandFundLocator= page.locator("//p[.='Lost & Found']");
+
+        this.GeneralInformationLocator = page.locator('(//span[@class="st-arrow"])[1]');
+        this.verifygeneralinformationLocator = page.locator('[class="tickets-fore-back"]');
+        this.clicktoppingupcardsLocator=page.locator('(//span[@class="st-arrow"])[2]');
+        this.verifytoppingupcardspageLocator=page.locator('[class="panel-body"]');
+        this.clickNationalMobilitycardLocator=page.locator('(//span[@class="st-arrow"])[3]');
+        this.verifyNationalMobilityLocator = page.locator('(//div[@class="st-content"])[3]');
 
 
     }
@@ -46,6 +79,55 @@ export default class TravelInfopage {
     async verifymetrotimings(){
         expect(this.verifymetrotimingLocator).toBeTruthy();
     }
+    async clickOnTicketBtn(){
+        await this.clickticketsLocator.click();
+    }
+    async verifyTicketpage(){
+        expect(this.verifyTicketsLocator).toBeTruthy();
+    }
+    async clickOnfacilitiesatStationBtn(){
+        await this.ClickfacilitisatstationLocator.click();
+    }
+    async verifyfacilitieStation(){
+        expect(this.verifyfacilitiesatstationLocator).toBeTruthy();
+    }
+    async clickOnParkingBtn(){
+        await this.clickParkingLocators.click();
+    }
+    async verifyparking(){
+        expect(this.verifyparkingpageLocator).toBeTruthy();
+    }
+    async clickOnSafetyPrecautionsBtn(){
+        await this.ClickSafetyPrecationsLocator.click();
+    }
+    async verifySafetyPrecaution(){
+        expect(this.verifySafetyPrecationsLocator).toBeTruthy();
+    }
+    async clickOnLostandFundBtn(){
+        await this.clickLostandFundLocator.click();
+    }
+    async verifylostandFund(){
+        expect(this.verifyLostandFundLocator).toBeTruthy();
+    }
+    async clickOnGeneralInformationBtn(){
+        await this.GeneralInformationLocator.click();
+    }
+    async verifyGeneralInformation(){
+        expect(this.verifygeneralinformationLocator).toBeTruthy();
+    }
+    async clickOntoppingupcardsBtn(){
+        await this.clicktoppingupcardsLocator.click();
+    }
+    async verifytoppingupcardspage(){
+        expect(this.verifytoppingupcardspageLocator).toBeTruthy();
+    }
+    async clickOnNationalMobilitycardBtn(){
+        await this.clickNationalMobilitycardLocator.click();
+    }
+    async verifynationalmobilitypage(){
+        expect(this.verifyNationalMobilityLocator).toBeTruthy();
+    }
+    
 
 
     }
--- tests/Travel-info-page.spec.ts
+++ tests/Travel-info-page.spec.ts
@@ -16,15 +16,49 @@ test('verify LIU shoud able to navigate to TravelInfo page and click on schemati
 
 
 
-test('verify LIU should able to navigate Fare rules, metroTimings, Tickets',async ({ page }) => {
+test('verify LIU should able to navigate Fare rules, metroTimings, Tickets, Facilitis at station, Facilities for specialiy abled, parking, Metro etiquette, safety Precauitions, Lost and Fund, Frequently asked Qns',async ({ page }) => {
     const home = new Homepage(page);
     const travelinfo = new TravelInfopage(page)
     await home.gotoLoginPage(data.url);
     await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnSchematicRoutemap();
     await travelinfo.VerifySchematicRoadmap();
+    await travelinfo.clickOnTravelInfoBtn();
     await travelinfo.ClickOnFareRulesBtn();
     await travelinfo.VerifyFareRules();
+    await travelinfo.clickOnTravelInfoBtn();
     await travelinfo.clickOnmetrotimingsBtn();
     await travelinfo.verifymetrotimings();
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnTicketBtn();
+    await travelinfo.verifyTicketpage();
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnfacilitiesatStationBtn();
+    await travelinfo.verifyfacilitieStation();
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnParkingBtn();
+    await travelinfo.verifyparking();
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnSafetyPrecautionsBtn();
+    await travelinfo.verifySafetyPrecaution();
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnLostandFundBtn();
+    await travelinfo.verifylostandFund();
+
+});
+
+test('verify should able to tickets navigation genaral information.',async ({ page }) => {
+    const home = new Homepage(page);
+    const travelinfo = new TravelInfopage(page)
+    await home.gotoLoginPage(data.url);
+    await travelinfo.clickOnTravelInfoBtn();
+    await travelinfo.clickOnTicketBtn();
+    await travelinfo.verifyTicketpage();
+    await travelinfo.clickOnGeneralInformationBtn();
+    await travelinfo.verifyGeneralInformation();
+    await travelinfo.clickOntoppingupcardsBtn();
+    await travelinfo.verifytoppingupcardspage();
+    await travelinfo.clickOnNationalMobilitycardBtn();
+    await travelinfo.verifynationalmobilitypage();
 
 });
